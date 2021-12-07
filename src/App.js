402259fcec70d105cef8b081af5989488032418e
shsv382 <shsv382@gmail.com>
2021-12-07 22:30:41 +0300

    Added collection-preview and -item components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsComponent = () => (<div><h1>HATS COMPONENT</h1></div>)
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} /> 
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} /> 
       </Switch>
     </div>
   );
