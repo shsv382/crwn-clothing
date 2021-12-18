3cf5ba8555e2510c28425b8f8fb3dbacf76b1414
shsv382 <shsv382@gmail.com>
2021-12-19 00:55:56 +0300

    Added Checkout page, Checkout-item component, updated cart to use selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -7,10 +7,11 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
 import { selectCartItems } from "../../redux/cart/cart.selectors";
+import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { 
@@ -23,7 +24,10 @@ const CartDropdown = ({ cartItems, history }) => (
                 )
             }
         </div>
-        <CustomButton onClick={() => history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
+        <CustomButton onClick={() => {
+            dispatch(toggleCartHidden());
+            history.push('/checkout');
+        }}>GO TO CHECKOUT</CustomButton>
     </div>
 )
 
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -21,3 +21,11 @@ export const selectCartItemsCount = createSelector(
         0
     )
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (total, item) => (total + item.price * item.quantity),
+        0
+    )
+)
