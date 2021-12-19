3b303b254faae48b0fb12fef43d9fee18af445c1
shsv382 <shsv382@gmail.com>
2021-12-19 22:32:24 +0300

    Prepared to add nested routes

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,29 +1,27 @@
 import React from "react";
+import { connect } from 'react-redux';
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+import { createStructuredSelector } from 'reselect';
+
 import './directory.styles.scss';
-import SECTIONS_DATA from "./sections.data.js";
+// import SECTIONS_DATA from "./sections.data.js";
 
 import MenuItem from "../menu-item/menu-item.component";
 
-class Directory extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            sections: SECTIONS_DATA
+const Directory = ({sections}) => (
+    <div className='directory-menu'>
+        {
+            sections.map(({id, ...otherSectionProps }) => (
+                <MenuItem key={id} 
+                            {...otherSectionProps} />
+            ))
         }
-    }
+    </div>
+)
 
-    render() {
-        return(
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({id, ...otherSectionProps }) => (
-                        <MenuItem key={id} 
-                                  {...otherSectionProps} />
-                    ))
-                }
-            </div>
-        )
-    }
-}
+const mapStateToProps = createStructuredSelector({
+    sections: selectDirectorySections
+})
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
